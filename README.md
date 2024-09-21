a6a5f3906583670deecd1c24ffd0db574dbd9a06
JoC0de <53140583+JoC0de@users.noreply.github.com>
2024-09-21 12:22:05 +0200

    Support additional plugins using '--plugin' command line argument (#1)
    * allow specifying additional plugins with '--plugin' command line argument
    * fix running when no '@prettier' directory exists
    * add workaround to remove the '(unchanged)' logs from the output

--- README.md
+++ README.md
@@ -19,8 +19,9 @@ Add this to your `.pre-commit-config.yaml`:
     -   id: prettier
 ```
 
-When using plugins with `prettier` you'll need to declare them under
-`additional_dependencies`. For example:
+When using `prettier` plugins you'll need to declare them under `additional_dependencies`.
+When the plugin name doesn't start with `@prettier/` you need to also add a `--plugin=` argument.
+For example:
 
 ```yaml
 -   repo: https://github.com/JoC0de/pre-commit-prettier
@@ -30,6 +31,9 @@ When using plugins with `prettier` you'll need to declare them under
         additional_dependencies:
         -   prettier@3.3.3
         -   '@prettier/plugin-xml@3.4.1'
+        -   'prettier-plugin-ini@1.2.0'
+        args:
+        -   --plugin=prettier-plugin-ini
 ```
 
 By default, all files are passed to `prettier`, if you want to limit the
--- run-prettier.js
+++ run-prettier.js
@@ -2,22 +2,51 @@
 
 "use strict";
 
-import { readFileSync, readdirSync } from "node:fs";
+import { readFileSync, readdirSync, existsSync } from "node:fs";
 import path from "node:path";
 import { run as runPrettier } from "prettier/internal/cli.mjs";
 
-const pluginsPath = path.resolve(process.env.NODE_PATH, "@prettier");
+const nodePath = path.resolve(process.env.NODE_PATH);
+const pluginsPath = path.resolve(nodePath, "@prettier");
 
-const plugins = readdirSync(pluginsPath, { withFileTypes: true });
+let plugins = [];
+
+if (existsSync(pluginsPath)) {
+  plugins = readdirSync(pluginsPath, { withFileTypes: true });
+}
+
+// if there is a plugin added using '--plugin=<plugin-name>' command line argument
+// we add them to the plugins list searching them inside the node folder
+for (let argumentIndex = process.argv.length - 1; argumentIndex >= 0; argumentIndex--) {
+  const argument = process.argv[argumentIndex];
+  if (!argument.startsWith("--plugin=")) {
+    continue;
+  }
+
+  const pluginName = argument.slice("--plugin=".length);
+  const matchingDirectories = readdirSync(nodePath, { withFileTypes: true }).filter(
+    (pluginPath) => pluginPath.isDirectory() && pluginPath.name.localeCompare(pluginName, "en", { sensitivity: "base" }) === 0
+  );
+
+  if (matchingDirectories.length === 0) {
+    continue;
+  }
+
+  // consume the command line argument
+  process.argv.splice(argumentIndex, 1);
+
+  plugins.push(
+    ...matchingDirectories.filter((directory) =>
+      plugins.every((existing) => directory.parentPath !== existing.parentPath && directory.name !== existing.name)
+    )
+  );
+}
 
 // neither single nor double quotes are supported around the path.
 const additionalArguments = plugins
   .filter((pluginPath) => pluginPath.isDirectory())
   .flatMap((pluginPath) => {
-    const pluginDirectoryPath = path.join(
-      pluginPath.parentPath,
-      pluginPath.name
-    );
+    const pluginDirectoryPath = path.join(pluginPath.parentPath, pluginPath.name);
     let pluginImportPath;
     try {
       const packageJsonPath = path.join(pluginDirectoryPath, "package.json");
@@ -27,10 +56,7 @@ const additionalArguments = plugins
         if (typeof packageJson.exports["."] === "string") {
           pluginImportPath = path.join(pluginDirectoryPath, packageJson.exports["."]);
         } else if (packageJson.exports["."].default) {
-          pluginImportPath = path.join(
-            pluginDirectoryPath,
-            packageJson.exports["."].default
-          );
+          pluginImportPath = path.join(pluginDirectoryPath, packageJson.exports["."].default);
         }
       }
 
@@ -44,9 +70,7 @@ const additionalArguments = plugins
         pluginImportPath = pluginDirectoryPath;
       }
     } catch (error) {
-      console.error(
-        `Error '${error}' reading or parsing package.json from '${pluginDirectoryPath}'`
-      );
+      console.error(`Error '${error}' reading or parsing package.json from '${pluginDirectoryPath}'`);
       pluginImportPath = pluginDirectoryPath;
     }
     return ["--plugin", pluginImportPath];
@@ -54,5 +78,16 @@ const additionalArguments = plugins
 
 // the first two items of process.argv are reserved (the node.exe and the current file name) so insert after them
 process.argv.splice(2, 0, ...additionalArguments);
+
+// Override stdout.write to filter message that end with "(unchanged)"
+// prettier currently logs messages when a file is not changed we are not interested in them
+const originalWrite = process.stdout.write;
+process.stdout.write = function (message, ...optionalParams) {
+  if (typeof message === "string" && message.includes(" (unchanged)")) {
+    return;
+  }
+
+  originalWrite.apply(process.stdout, [message, ...optionalParams]);
+};
 
 runPrettier();
